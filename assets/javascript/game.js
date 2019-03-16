82735861d1f015678f8a2a02800d3c214f46fd53
apizarras <angelalee.p@gmail.com>
2019-03-15 20:31:13 -0500

    git@github.com:apizarras.magicalitems

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var min = 19;
+var max = 120;
+
+var targetNumber = Math.floor(Math.random() * (+max - +min)) + +min;
+console.log(targetNumber);
+
+var itemMin = 1;
+var itemMax = 12;
+
+var item1 = Math.floor(Math.random() * (+itemmax - +itemmin) + +itemmin;
+var item2 = Math.floor(Math.random() * (+itemmax - +itemmin) + +itemmin;
+var item3 = Math.floor(Math.random() * (+itemmax - +itemmin) + +itemmin;
+var item4 = Math.floor(Math.random() * (+itemmax - +itemmin) + +itemmin;
+
+document.getElementById("item1-points").innerHTML = item1;
+document.getElementById("item2-points").innerHTML = item2;
+document.getElementById("item3-points").innerHTML = item3;
+document.getElementById("item4-points").innerHTML = item4;
+
