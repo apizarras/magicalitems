deaa4112d5158db722c76393adcbd505017ff2c7
apizarras <angelalee.p@gmail.com>
2019-03-19 13:46:03 -0500

    git@github.com:apizarras.magicalitems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,32 @@
+
+
+var playerScore = 0;
+var win = 0;
+$("#win").text(win);
+
 var min = 19;
-var max = 120;
-let randomItemNumber;
+var max = 120; 
+var targetNumber = Math.floor(Math.random() * (max - min+1)) + min;
+console.log(targetNumber);
+$("#target-number").text(targetNumber);
+var item1 = Math.floor(Math.random() *12 +1);
+console.log(item1);
+var item2 = Math.floor(Math.random() *12 +1);
+console.log(item2);
+var item3 = Math.floor(Math.random() *12 +1);
+console.log(item3);
+var item4 = Math.floor(Math.random() *12 +1);
+console.log(item4);
+
+$(document).ready(function() {
+    $(".win-loss-message").hide();
+
 //function to generate a random target number within the min/max
 function randomTargetNumber() {
+    var min = 19;
+    var max = 120;  
     var targetNumber = Math.floor(Math.random() * (max - min+1)) + min;
+    return targetNumber;
     console.log(targetNumber);
 }
 //function to generate a random number with the clickable items min/mas
@@ -16,42 +39,113 @@ function genRandNumber() {
 }
 //reset function
 function restart() {
+    $(".win-loss-message").hide();
     //generate numbers
-    var target = randomTargetNumber();
-    var item1 = genRandNumber();
-    console.log("New Item 1: " + item1);
-    var item2 = genRandNumber();
-    console.log("New Item 2: " + item2);
-    var item3 = genRandNumber();
-    console.log("New Item 3: " + item3);
-    var item4 = genRandNumber();
-    console.log("New Item 4: " + item4);
-
+    targetNumber = randomTargetNumber();
+    $("#target-number").text(targetNumber);
+    console.log(targetNumber);
+    item1 = genRandNumber();
+    item2 = genRandNumber();
+    item3 = genRandNumber();
+    item4 = genRandNumber();
+    
     //assign values to the img
     $("#item1").attr("value", item1);
+    console.log("New Item 1: " + item1);
     $("#item2").attr("value", item2);
+    console.log("New Item 2: " + item2);
     $("#item3").attr("value", item3);
+    console.log("New Item 3: " + item3);
     $("#item4").attr("value", item4);
-    
+    console.log("New Item 4: " + item4);
+
+    //reset playerScore
+    playerScore = 0;
+    $(".player-score").html(playerScore);
+    console.log(playerScore);
 }
-// START game Loop      function startGame () {
 
-//generate Target and magical item values
-    restart();
+function win() {
+        $(".win-loss-message").show();
+        win++;
+        console.log(win);
+        $("#win").append(win);
+        restart();
+}
 
-    var currentScore = 0;
+function lost() {
+        $(".win-loss-message").show();
+        restart();
+}
+
     $("#item1").on("click", function(event) {
         console.log("clicked");
-
-            if(item1){
-                var currentScore = currentScore + item1;
-                $(".player-score").html(currentScore);
-            } else if (currentScore!=0 && (item2)) {
-                currentScore + item2;
+        playerScore = playerScore + item1;
+        console.log(playerScore);
+        console.log(targetNumber);
+        $(".player-score").text(playerScore);
+            if (playerScore == targetNumber) {
+                console.log("THINKS YOU WON");
+                win();
+            } else if (playerScore > targetNumber) {
+                console.log("THINKS YOU LOST");
+                lost();
             }
-    
-            
-    });
+            else if (playerScore < targetNumber) {
+                console.log("Still Playing");
+                } 
+    })
+
+    $("#item2").on("click", function(event) {
+        console.log("clicked");
+        playerScore = playerScore + item2;
+        console.log("current player score: " + playerScore);
+        $(".player-score").text(playerScore);
+            if (playerScore == targetNumber) {
+                console.log("THINKS YOU WON");
+                win();
+            } else if (playerScore > targetNumber) {
+                console.log("THINKS YOU LOST");
+                lost();
+            }
+            else if (playerScore < targetNumber) {
+                console.log("Still Playing");
+                } 
+    })
+    $("#item3").on("click", function(event) {
+        console.log("clicked");
+        playerScore = playerScore + item3;
+        console.log("current player score: " + playerScore);
+        $(".player-score").text(playerScore);
+            if (playerScore == targetNumber) {
+                console.log("THINKS YOU WON");
+                win();
+            } else if (playerScore > targetNumber) {
+                console.log("THINKS YOU LOST");
+                lost();
+            }
+            else if (playerScore < targetNumber) {
+                console.log("Still Playing");
+                } 
+    })
+    $("#item4").on("click", function(event) {
+        console.log("clicked");
+        playerScore = playerScore + item4;
+        console.log("current player score: " + playerScore);
+        $(".player-score").text(playerScore);
+            if (playerScore == targetNumber) {
+                console.log("THINKS YOU WON");
+                win();
+            } else if (playerScore > targetNumber) {
+                console.log("THINKS YOU LOST");
+                lost();
+            }
+            else if (playerScore < targetNumber) {
+                console.log("Still Playing");
+                } 
+    })
+
+
 
 // END game loop     }
-
+});
